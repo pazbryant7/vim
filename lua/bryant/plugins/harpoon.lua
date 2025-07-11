952620b8296a77a2fcd709fa0393fb1db87b87f1
Bryant Paz <pazbryant7@outlook.com>
2025-07-11 15:07:19 -0600

    feat(harpoon): improve notification

--- lua/bryant/plugins/harpoon.lua
+++ lua/bryant/plugins/harpoon.lua
@@ -14,7 +14,6 @@ return {
 
     -- stylua: ignore start
 		return {
-      { '<c-y>', function() harpoon:list():add() end, desc = 'Harpoon add', },
       { '<m-e>', function() harpoon:list():select(3) end, desc = 'Harpoon go to third harpoon', },
       { '<m-a>', function() harpoon:list():select(1) end, desc = 'Harpoon go to first harpoon', },
       { '<m-u>', function() harpoon:list():select(4) end, desc = 'Harpoon go to fourd harpoon', },
@@ -22,6 +21,7 @@ return {
       { '<c-l>', function() harpoon:list():next({ ui_nav_wrap = true }) end, desc = 'Harpoon next harpoon', },
       { '<c-h>', function() harpoon:list():prev({ ui_nav_wrap = true }) end, desc = 'Harpoon previous harpoon', },
       { '<c-e>', function() harpoon.ui:toggle_quick_menu(harpoon:list()) end, desc = 'Harpoon toggle quick menu', },
+      { '<c-y>', function() harpoon:list():add() vim.notify("file added to harpoon",vim.log.levels.INFO, { title = "Harpoon" }) end, desc = 'Harpoon add', },
 		}
 	end,
 }
