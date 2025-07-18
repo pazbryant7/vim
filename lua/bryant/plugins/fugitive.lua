57227628e5ab8b708177c673b2e4b2b360595b69
Bryant Paz <pazbryant7@outlook.com>
2025-07-18 01:38:19 -0600

    feat(fugitive): update default configuration

--- lua/bryant/plugins/fugitive.lua
+++ lua/bryant/plugins/fugitive.lua
@@ -1,30 +1,9 @@
 return {
 	'tpope/vim-fugitive',
+	cmd = { 'Gclog', 'Git' },
 	keys = {
+    -- stylua: ignore start
 		{ '<c-g>', ':tab Git <cr>', desc = 'Fugitive init' },
-		{
-			'<leader>gd',
-			function()
-				vim.cmd([[Gvdiffsplit!]])
-				vim.notify('Resolving git conflicts', vim.log.levels.INFO, { title = 'Fugitive' })
-			end,
-			desc = 'Fugitive Diff Split',
-		},
-		{
-			'<leader>go',
-			function()
-				vim.cmd([[diffget //2]])
-				vim.notify('Git ours selected', vim.log.levels.INFO, { title = 'Fugitive' })
-			end,
-			desc = 'Fugitive Ours Changes',
-		},
-		{
-			'<leader>gt',
-			function()
-				vim.cmd([[diffget //3]])
-				vim.notify('Git theirs selected', vim.log.levels.INFO, { title = 'Fugitive' })
-			end,
-			desc = 'Fugitive Theirs Changes',
-		},
+		{ '<leader>gl', ':Gclog <cr>', desc = 'Fugitive git log' },
 	},
 }
