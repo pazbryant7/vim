0267b5d3f485cdc5bb1fdcb71d9a357272cbd730
Bryant Paz <pazbryant7@outlook.com>
2025-08-04 19:11:33 -0600

    feat(oil): hide stastus bar when exit oil

--- after/ftplugin/oil.lua
+++ after/ftplugin/oil.lua
@@ -2,3 +2,10 @@ local v = vim
 local o = v.opt_local
 
 o.laststatus = 2
+
+v.api.nvim_create_autocmd('BufWipeout', {
+	pattern = '<buffer>',
+	callback = function()
+		v.opt.laststatus = 0
+	end,
+})
