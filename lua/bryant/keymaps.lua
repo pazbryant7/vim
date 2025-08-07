f68bd0ce928b5b506b2b3130299a6d7188de0376
Bryant Paz <pazbryant7@outlook.com>
2025-08-07 10:44:42 -0600

    feat(keymaps): update default maps

--- lua/bryant/keymaps.lua
+++ lua/bryant/keymaps.lua
@@ -3,7 +3,7 @@ local v = vim
 local map = v.keymap.set
 
 -- Manage exit and saves
-map('n', '<c-f>', '<cmd>w<CR>', { desc = 'Save current buffer' })
+map('n', '<c-f>', '<cmd>update<CR>', { desc = 'Save current buffer' })
 map('n', '<leader>f', '<cmd>wa<CR>', { desc = 'Save all buffers' })
 map('n', '<leader>q', '<cmd>q!<CR>', { desc = 'Quit without save' })
 map('n', '<leader>Q', '<cmd>qa!<CR>', { desc = 'Quit all without save' })
