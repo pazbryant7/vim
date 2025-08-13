c38de72b953a3bcc4745094fce1f6ba2607cf934
Bryant Paz <pazbryant7@outlook.com>
2025-08-12 19:49:24 -0600

    feat(zen-mode): new plugin added

---
+++ lua/bryant/plugins/zenmode.lua
@@ -0,0 +1,24 @@
+return {
+	'folke/zen-mode.nvim',
+	opts = {
+		window = {
+			backdrop = 100, -- shade the backdrop of the Zen window. Set to 1 to keep the same as Normal
+			width = 80, -- width of the Zen window
+		},
+		plugins = {
+			options = {
+				enabled = false,
+			},
+			tmux = { enabled = true },
+			todo = { enabled = false },
+		},
+	},
+	keys = {
+		{
+			'zm',
+			mode = 'n',
+			'<cmd>ZenMode<CR>',
+			desc = 'Zen mode',
+		},
+	},
+}
