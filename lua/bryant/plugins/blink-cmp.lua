dd9ba0a025f56595eaceb61e8082445d7e37773a
Bryant Paz <pazbryant7@outlook.com>
2025-08-14 21:54:18 -0600

    feat: up to date with main configuration

--- lua/bryant/plugins/blink-cmp.lua
+++ lua/bryant/plugins/blink-cmp.lua
@@ -25,6 +25,7 @@ return {
 			documentation = { auto_show = false },
 			accept = { auto_brackets = { enabled = true } },
 			menu = {
+				auto_show = true,
 				draw = {
 					columns = {
 						{ 'label', 'label_description', gap = 1 },
