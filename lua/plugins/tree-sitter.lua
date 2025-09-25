1bb2711463afe71bac7d8a636bf677b70eefbe7b
Bryant Paz <pazbryant7@outlook.com>
2025-09-25 14:08:37 -0600

    feat(plugins): remove unused ones

--- lua/plugins/tree-sitter.lua
+++ lua/plugins/tree-sitter.lua
@@ -28,15 +28,10 @@ return {
 			indent = { enable = false },
 			highlight = {
 				enable = true,
-				disable = function(lang, buf)
+				disable = function(_, buf)
 					local max_filesize = 100 * 1024 -- 100 KB
-					local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
+					local ok, stats = pcall(vim.uv.fs_stat, vim.api.nvim_buf_get_name(buf))
 					if ok and stats and stats.size > max_filesize then
-						vim.notify(
-							'File larger than 100KB treesitter disabled for performance',
-							vim.log.levels.WARN,
-							{ title = 'Treesitter' }
-						)
 						return true
 					end
 				end,
