ddfb18c624c4fc9a2aff49e85fef261c2ab3317f
Bryant Paz <pazbryant7@outlook.com>
2025-09-30 12:00:26 -0600

    feat: load default options at start

--- init.lua
+++ init.lua
@@ -1,6 +1,7 @@
+require('config.init')
+
 require('config.lazy')
 require('config.colors')
 require('config.autocmd')
 require('config.keymaps')
 require('config.usercmd')
-require('config.options')
