56b07b0cb543191a72afe5fd8ec50c4e3faaac03
Bryant Paz <pazbryant7@outlook.com>
2025-10-21 19:12:41 -0600

    feat(transarent): toggle bind

--- lua/plugins/transparent.lua
+++ lua/plugins/transparent.lua
@@ -1,3 +1,5 @@
+local enabled = false
+
 return {
 	'xiyaowong/transparent.nvim',
 	cmd = { 'TransparentToggle', 'TransparentEnable' },
@@ -44,5 +46,21 @@ return {
 		extra_groups = {},
 		exclude_groups = {},
 		on_clear = function() end,
+	},
+	keys = {
+		{
+			'<leader>tt',
+			function()
+				if enabled then
+					vim.cmd('TransparentDisable')
+					vim.notify('Transparency disabled', vim.log.levels.INFO)
+				else
+					vim.cmd('TransparentEnable')
+					vim.notify('Transparency enabled', vim.log.levels.INFO)
+				end
+				enabled = not enabled
+			end,
+			desc = 'Toggle transparency',
+		},
 	},
 }
